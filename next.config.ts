73590754a357a4c296f6b94e402d31eac19ceb7a
compumaxhosting <compumaxhost@gmail.com>
2025-05-09 22:27:22 +0530

    about

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: "/about",
-        destination: "/about-us",
+        source: "/about-us",
+        destination: "/about",
         permanent: true,
       },
     ];
