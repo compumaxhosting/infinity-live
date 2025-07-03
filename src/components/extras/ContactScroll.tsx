367cbc1707665f3e5fffc6a82780b92de81fbaeb
compumaxhosting <compumaxhost@gmail.com>
2025-07-03 21:28:12 +0530

    fixed buttons

--- src/components/extras/ContactScroll.tsx
+++ src/components/extras/ContactScroll.tsx
@@ -12,7 +12,7 @@ const ContactScroll = () => {
     <Link href="" to="contact-us" smooth={true} duration={1000}>
       <button
         style={{ fontFamily: "var(--font-forum)" }}
-        className="fixed z-10 bottom-16 left-6 bg-primary text-white p-3 px-3 sm:px-8 md:px-10 rounded-full shadow-lg flex items-center space-x-2 hover:bg-stone-900 transition-all duration-300"
+        className="fixed z-10 bottom-6 left-6 bg-primary text-white p-3 px-3 sm:px-8 md:px-10 rounded-full shadow-lg flex items-center space-x-2 hover:bg-stone-900 transition-all duration-300"
       >
         <ReceiptText className="w-4 h-4" />
         <span className="inline text-md md:text-xl">Get a Quote</span>
